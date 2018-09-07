74f8dc1bc293d68fa98c72c18f3b9056670fbe74
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-09-07 14:16:31 -0300

    Criado servidor com Express e NodeJS para servir a aplicação

---
+++ server.js
@@ -0,0 +1,8 @@
+let express = require('express');
+let app = express();
+
+const port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => console.log(`Server is running in http://localhost:${port}`));
