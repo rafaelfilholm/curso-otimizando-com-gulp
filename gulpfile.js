e47524eedd38fe4dffa745841e82189643720dd8
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-09-07 13:46:45 -0300

    Adicionado task para compilar o less do Bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,12 @@
-const gulp = require('gulp');
+let gulp = require('gulp');
+let customizeBootstrap = require('gulp-customize-bootstrap');
+let less = require('gulp-less');
+ 
+gulp.task('default', ['compileBootstrap']);
 
-gulp.task('default', []);
+gulp.task('compileBootstrap', function() {
+  return gulp.src('node_modules/bootstrap/less/bootstrap.less')
+    .pipe(customizeBootstrap('./src/styles/less/*.less'))
+    .pipe(less())
+    .pipe(gulp.dest('./dist/css/'));
+});
