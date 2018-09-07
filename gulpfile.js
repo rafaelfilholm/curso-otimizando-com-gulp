0aa3491aedc1af74ec845dcdc1a253ba9b3e5d96
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-09-07 14:30:31 -0300

    Adicionado jquery e task para concatenar todas as libs

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 let gulp = require('gulp');
+let gulpConcat = require('gulp-concat');
 let customizeBootstrap = require('gulp-customize-bootstrap');
 let less = require('gulp-less');
  
-gulp.task('default', ['compileBootstrap']);
+gulp.task('default', ['compileBootstrap', 'lib']);
 
-gulp.task('compileBootstrap', function() {
-  return gulp.src('node_modules/bootstrap/less/bootstrap.less')
+gulp.task('compileBootstrap', () => {
+  gulp.src('./node_modules/bootstrap/less/bootstrap.less')
     .pipe(customizeBootstrap('./src/styles/less/*.less'))
     .pipe(less())
     .pipe(gulp.dest('./public/dist/css/'));
+});
+
+gulp.task('lib', () => {
+	gulp.src(['./node_modules/jquery/dist/jquery.min.js'])
+		.pipe(gulpConcat('bundle.js'))
+		.pipe(gulp.dest('./public/dist/js/'));
 });
