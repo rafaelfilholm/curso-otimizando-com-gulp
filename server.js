56ba1358011e3f416a0335f96f5c318379345985
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-09-07 14:22:44 -0300

    Alterada mensagem de servidor iniciado com sucesso

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, () => console.log(`Server is running in http://localhost:${port}`));
+app.listen(port, () => console.log(`Server is running in port ${port}`));
