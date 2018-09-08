3f64af42ca9212db8545f4308b1bc2591c58489c
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-09-08 11:10:34 -0300

    Alterado nome do concat de libs js

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,6 @@ gulp.task('compileBootstrap', () => {
 
 gulp.task('lib', () => {
 	gulp.src(['./node_modules/jquery/dist/jquery.min.js'])
-		.pipe(gulpConcat('bundle.js'))
+		.pipe(gulpConcat('lib.js'))
 		.pipe(gulp.dest('./public/dist/js/'));
 });
